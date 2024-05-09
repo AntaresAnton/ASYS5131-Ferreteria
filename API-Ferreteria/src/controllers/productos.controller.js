7e1918ffa6ae2c578db2efec2ce482596ad93f48
AntaresAnton <patricioquintanilladx@gmail.com>
2024-05-09 19:54:35 -0400

    .

--- API-Ferreteria/src/controllers/productos.controller.js
+++ API-Ferreteria/src/controllers/productos.controller.js
@@ -9,11 +9,14 @@ const obtenerProducto = async (req, res) => {
         const connection = await getConnection();
         const [result] = await connection.query(
         `SELECT 
-        p.*, c.nombre_categoria
-    FROM 
-        Productos p
-    INNER JOIN 
-        Categoria c ON p.id_categoria = c.id;
+        PROD.id_producto, 
+        PROD.sku,
+        PROD.nombre_producto,
+        PROD.precio, ROUND(PROD.precio / DIVI.valor,2) AS precio_en_dolares,
+        DIVI.valor as valor_dolar_dia,
+        DIVI.actualizado_el as dolar_actualizado
+        FROM productos PROD 
+        INNER JOIN divisas DIVI ON PROD.codigo_divisa = DIVI.codigo_divisa;
      `);
         
         // Verificar si hay resultados
